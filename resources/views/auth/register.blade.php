f88e67a2ad0d23f6738ee691f3689346ffeb179c
Alan <alanmannon@gmail.com>
2020-12-20 15:46:01 -0600

    register values will now stay after submit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
       @csrf
       <div class="mb-4">
         <label for="name" class="sr-only">Name</label>
-        <input type="text" name="name" id="name" placeholder="Your name" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="">
+        <input type="text" name="name" id="name" placeholder="Your name" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="{{ old('name') }}">
 
         @error('name')
         <div class="text-red-500 mt-2 text-sm">
@@ -18,7 +18,7 @@
 
       <div class="mb-4">
         <label for="username" class="sr-only">Username</label>
-        <input type="text" name="username" id="username" placeholder="Username" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('username') border-red-500 @enderror"" value="">
+        <input type="text" name="username" id="username" placeholder="Username" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('username') border-red-500 @enderror" value="{{ old('username') }}">
 
         @error('username')
         <div class=" text-red-500 mt-2 text-sm">
@@ -29,7 +29,7 @@
 
       <div class="mb-4">
         <label for="email" class="sr-only">Email</label>
-        <input type="email" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror"" value="">
+        <input type="email" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
 
         @error('email')
         <div class=" text-red-500 mt-2 text-sm">
@@ -40,7 +40,7 @@
 
       <div class="mb-4">
         <label for="password" class="sr-only">Password</label>
-        <input type="password" name="password" id="password" placeholder="Choose a password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror"" value="">
+        <input type="password" name="password" id="password" placeholder="Choose a password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror" value="">
 
         @error('password')
         <div class=" text-red-500 mt-2 text-sm">
@@ -51,7 +51,7 @@
 
       <div class="mb-4">
         <label for="password_confirmation" class="sr-only">Password again</label>
-        <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password_confirmation') border-red-500 @enderror"" value="">
+        <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password_confirmation') border-red-500 @enderror" value="">
 
         @error('password_confirmation')
         <div class=" text-red-500 mt-2 text-sm">
