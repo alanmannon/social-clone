2a3e29410ac2584e69584e9acf6492cbe8ff0881
Alan <alanmannon@gmail.com>
2020-12-20 22:20:43 -0600

    logout function working

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\DashboardController;
 
 // You can remove the route name because it will inherit from the first one
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
+
+Route::post('/logout', [LogoutController::class, 'index'])->name('logout');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
