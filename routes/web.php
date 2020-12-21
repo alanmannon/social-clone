7f42626c62416f30cf752a41086a8e6b678aea63
Alan <alanmannon@gmail.com>
2020-12-20 21:41:40 -0600

    users can now create accounts, redirects to dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 // You can remove the route name because it will inherit from the first one
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/posts', function () {
     return view('posts.index');
