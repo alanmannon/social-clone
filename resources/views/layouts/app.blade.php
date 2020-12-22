a36eea0f9b650a2cbb35233900e86c67398f2af2
Alan <alanmannon@gmail.com>
2020-12-21 20:45:31 -0600

    posts model and relations created. user can now create posts. post index page has pagination

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,13 +19,13 @@
         <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
       </li>
       <li>
-        <a href="" class="p-3">Post</a>
+        <a href="{{ route('posts') }}" class="p-3">Posts</a>
       </li>
     </ul>
     <ul class="flex items-center">
       @auth
       <li>
-        <a href="" class="p-3">USER</a>
+        <a href="" class="p-3">{{ auth()->user()->name }}</a>
       </li>
       <li>
         <form action="{{ route('logout') }}" method="post" class="p-3 inline">
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 
 // You can remove the route name because it will inherit from the first one
 Route::get('/', function () {
@@ -21,6 +22,5 @@ Route::post('/logout', [LogoutController::class, 'index'])->name('logout');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::post('/posts', [PostController::class, 'store']);
