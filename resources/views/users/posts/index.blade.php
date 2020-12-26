2ad6fcb32be3da98b38dc4e56bacbadbf11971ca
Alan <alanmannon@gmail.com>
2020-12-26 11:35:33 -0600

    created new component pages to make code easier to reuse

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -3,7 +3,14 @@
 @section('content')
 <div class="flex justify-center">
   <div class="w-8/12 bg-white p-6 rounded-lg">
-    {{ $user->name }}
+    @if ($posts->count())
+    @foreach ($posts as $post)
+    <x-post :post="$post" />
+    @endforeach
+    {{ $posts->links() }}
+    @else
+    <p>{{ $user->name }} does not have any posts.</p>
+    @endif
   </div>
 </div>
 @endsection
