8cf628b7be6c584d5f36bcbe58377f9ac2c0bc10
Alan <alanmannon@gmail.com>
2020-12-26 13:20:33 -0600

    only logged in users will now see the text post to post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,11 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth'])->only('store', 'destroy');
+    }
+
     public function index()
     {
         $posts = Post::latest()->with('user', 'likes')->paginate(10);
